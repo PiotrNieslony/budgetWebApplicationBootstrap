df5f484fd0f39e2904be64beb3b1a97ad338362e
PiotrNieslony <piotr.nieslony.programista@gmail.com>
2019-01-10 14:12:51 +0100

    config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    return [
-      //database
-        'host' => 'localhost',
-        'db' => 'nies_budget',
-        'user' => 'nies_budget',
-        'pass' => '&&2BmMDY0YkD'
-    ];
+return [
+  //database
+    'host' => 'localhost',
+    'db' => 'budget',
+    'user' => 'root',
+    'pass' => ''
+];
