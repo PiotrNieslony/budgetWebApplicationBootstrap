fb7c81fd5a4ba4132175b64694fbae8f88088d0f
PiotrNieslony <piotr.nieslony.programista@gmail.com>
2019-01-14 13:50:21 +0100

    start page has been rebuilt

---
+++ templates/modal-print-screen.php
@@ -0,0 +1,14 @@
+<?php if(!isset($budget)) die();?>
+<div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Zrzut ekranu<h4>
+      </div>
+      <div class="modal-body">
+        <img src="img/screen-shot.png" class="img-responsive" alt="Zrzut ekranu aplikacji">
+      </div>
+    </div>
+  </div>
+</div>
